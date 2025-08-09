c4e544e66a31b87cce47e06e6829f0c970946125
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2025-08-09 20:11:04 +0200

    refactor: simplify scripts

--- dist/routeutil.js
+++ dist/routeutil.js
@@ -1,2 +1,137 @@
-function f(n){return n.replace(/[.*+?^${}()|[\]\\]/g,"\\$&")}function l(n,...t){let e=[];for(let r=0;r<n.length;r++){e.push(f(n[r]));let s=t[r];typeof s=="string"?e.push(f(s)):s instanceof RegExp&&e.push(s.source)}if(!e[0].startsWith("#/"))throw new Error(`Routes must start with "#/"; route is ${e.join("")}`);return new RegExp(`^${e.join("")}$`)}function u(n){return new RegExp(`(?<${n}>\\w+)`)}var a=class{#t=new Map;#r=void 0;#s=void 0;#e=void 0;#n=new AbortController;constructor(t){this.#e=t?.startAt}connect(){return addEventListener("popstate",()=>{this.#i()},{signal:this.#n.signal}),this.#e&&!location.hash?location.hash=this.#e:this.#i(),this}disconnect(){this.#n.abort()}on(t,e){return(Array.isArray(t)?t:[t]).forEach(s=>this.#t.set(s,e)),this}off(t){return(Array.isArray(t)?t:[t]).forEach(r=>this.#t.delete(r)),this}fallback(t){return this.#r=t,this}afterEach(t){return this.#s=t,this}#i(t=location.hash){let e,r,s,o;for(let[i,c]of this.#t.entries())if(typeof i=="string"&&t===i?r=c:i instanceof RegExp&&(s=t.match(i))&&(o={...s.groups},r=c),r){e=i;break}let h={url:t,params:o??{},route:e};(r??this.#r)?.(h),this.#s?.(h)}};export{a as default,u as param,l as route};
-//# sourceMappingURL=routeutil.js.map
+// src/routeutil.js
+function escapeRegex(val) {
+  return val.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
+function route(segments, ...values) {
+  const buf = [];
+  for (let i = 0; i < segments.length; i++) {
+    buf.push(escapeRegex(segments[i]));
+    const val = values[i];
+    if (typeof val === "string") buf.push(escapeRegex(val));
+    else if (val instanceof RegExp) buf.push(val.source);
+  }
+  if (!buf[0].startsWith("#/")) {
+    throw new Error(`Routes must start with "#/"; route is ${buf.join("")}`);
+  }
+  return new RegExp(`^${buf.join("")}$`);
+}
+function param(name) {
+  return new RegExp(`(?<${name}>\\w+)`);
+}
+var Router = class {
+  /** @type {Map<RouteDef, RouteHandler>} */
+  #handlers = /* @__PURE__ */ new Map();
+  /** @type {RouteHandler | undefined} */
+  #fallbackHandler = void 0;
+  /** @type {RouteHandler | undefined} */
+  #afterEachHandler = void 0;
+  /** @type {string | undefined} */
+  #startAt = void 0;
+  #controller = new AbortController();
+  /**
+   * @param {object} [opts]
+   * @param {string} [opts.startAt] If set, the router will initially navigate
+   *  to the specified path if the locations' hash is empty when the router
+   *  connects.
+   */
+  constructor(opts) {
+    this.#startAt = opts?.startAt;
+  }
+  /**
+   * Performs the initial routing after the routes have been initialized.
+   *
+   * @returns {Router} The router instance for chaining
+   */
+  connect() {
+    addEventListener(
+      "popstate",
+      () => {
+        this.#exec();
+      },
+      { signal: this.#controller.signal }
+    );
+    if (this.#startAt && !location.hash) location.hash = this.#startAt;
+    else this.#exec();
+    return this;
+  }
+  /**
+   * Stops route handling. Note that it cannot be restarted, you will need to
+   * create a new router if you need it again.
+   */
+  disconnect() {
+    this.#controller.abort();
+  }
+  /**
+   * Adds a new route handler. If an identical handler already exists, the
+   * existing handler is replaced.
+   *
+   * @param {RouteDef | RouteDef[]} route Path of the route
+   * @param {RouteHandler} handler Function to be called when the route is matched
+   * @returns {Router} The router instance for chaining
+   */
+  on(route2, handler) {
+    const routeArr = Array.isArray(route2) ? route2 : [route2];
+    routeArr.forEach((i) => this.#handlers.set(i, handler));
+    return this;
+  }
+  /**
+   * Removes an existing route handler.
+   *
+   * @param {RouteDef | RouteDef[]} route
+   * @returns {Router} The router instance for chaining
+   */
+  off(route2) {
+    const routeArr = Array.isArray(route2) ? route2 : [route2];
+    routeArr.forEach((i) => this.#handlers.delete(i));
+    return this;
+  }
+  /**
+   * This handler will be called if no route matches.
+   *
+   * @param {RouteHandler} handler
+   * @returns {Router} The router instance for chaining
+   */
+  fallback(handler) {
+    this.#fallbackHandler = handler;
+    return this;
+  }
+  /**
+   * This handler will be called after any navigation (including when no route
+   * matched).
+   *
+   * @param {RouteHandler} handler
+   * @returns {Router} The router instance for chaining
+   */
+  afterEach(handler) {
+    this.#afterEachHandler = handler;
+    return this;
+  }
+  /**
+   * Parses the URL to a route and runs handler associated with the route.
+   *
+   * @param {string} [url] The new URL. Defaults to `location.hash`.
+   */
+  #exec(url = location.hash) {
+    let route2, handler, match, params;
+    for (const [k, v] of this.#handlers.entries()) {
+      if (typeof k === "string" && url === k) {
+        handler = v;
+      } else if (k instanceof RegExp && (match = url.match(k))) {
+        params = { ...match.groups };
+        handler = v;
+      }
+      if (handler) {
+        route2 = k;
+        break;
+      }
+    }
+    const resolved = { url, params: params ?? {}, route: route2 };
+    (handler ?? this.#fallbackHandler)?.(resolved);
+    this.#afterEachHandler?.(resolved);
+  }
+};
+export {
+  Router as default,
+  param,
+  route
+};
